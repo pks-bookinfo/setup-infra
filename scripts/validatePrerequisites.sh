422d5e9bf201b6b4a7c7b8548fbd4b5aa98988e6
akirasoft <akirasoft@gmail.com>
2019-03-19 13:27:10 -0500

    we don't need this stuff for pks, env will exist already

--- scripts/validatePrerequisites.sh
+++ scripts/validatePrerequisites.sh
@@ -41,49 +41,6 @@ then
 fi
 echo "ok"
 
-echo -n "Validating AWS cli ... "
-type aws &> /dev/null
-if [ $? -ne 0 ]
-then
-    echo "Error"
-    echo ">>> Missing 'aws CLI'"
-    echo ""
-    exit 1
-fi
-echo "ok"
-
-echo -n "Validating terraform ... "
-type aws &> /dev/null
-if [ $? -ne 0 ]
-then
-    echo "Error"
-    echo ">>> Missing 'terraform'"
-    echo ""
-    exit 1
-fi
-echo "ok"
-
-echo -n "Validating aws-iam-authenticator utility ... "
-type aws-iam-authenticator &> /dev/null
-if [ $? -ne 0 ]
-then
-    echo "Error"
-    echo ">>> Missing 'aws-iam-authenticator'"
-    echo ""
-    exit 1
-fi
-echo "ok"
-
-echo -n "Validating AWS cli is configured ... "
-export AWS_STS_USER=$(aws sts get-caller-identity | jq -r '.UserId')
-if [ -z $AWS_STS_USER ]
-then
-    echo ">>> Unable to locate credentials. You can configure credentials by running \"aws configure\"."
-    echo ""
-    exit 1
-fi
-echo "...ok - AWS cli is configured with UserId: $AWS_STS_USER"
-
 echo "----------------------------------------------------"
 echo Validation of pre-requisites complete.
 echo "----------------------------------------------------"
